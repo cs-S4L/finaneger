be2df5332fd4ef12a7bdae5f849d89fe0470f93a
Christopher <ChristopherSchmitz96@web.de>
2020-03-19 16:57:00 +0100

    Backdrop component implemented

---
+++ src/vue/components/CompBackdrop.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="comp-backdrop" class="backdrop" :class="visibleClass">
+        <div class="inner">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        visible: {}
+    },
+    components: {},
+    computed: {
+        visibleClass() {
+            console.log(this.visible);
+            if (this.visible) {
+                return "test";
+            } else {
+                return "hide";
+            }
+        }
+    },
+    methods: {}
+};
+</script>
+
+<style></style>
